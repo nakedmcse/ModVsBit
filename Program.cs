1adacd37611be058c29dd979083b487d06fe5832
Walker Aldridge <daldridge@lairdscomputer.com>
2023-11-13 14:09:11 -0600

    Add C# and Python versions

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+class ProgramClass {
+static volatile HashSet<int> numbers = new HashSet<int>();
+static List<bool> resultTracker = new List<bool>();
+
+static void Main(string[] args)
+    {
+        var timer = Stopwatch.StartNew();
+        for (int j = 0; j < 10; j++)
+        {
+            bool[] results = new bool[100000 * 2];
+            Console.WriteLine("Building test array");
+            var testArray = GenerateArray(100000);
+
+            Console.WriteLine("Testing Modulus Odd/Even");
+            timer.Restart();
+            for (int k = 0; k < testArray.Length; k++)
+            {
+                int i = testArray[k];
+                results[k] = EvenMod(i);
+            }
+            timer.Stop();
+            Console.WriteLine("Modulus took: " + timer.Elapsed);
+
+            timer.Reset();
+
+            testArray = GenerateArray(100000);
+            Console.WriteLine("Testing Bitwise Odd/Even");
+            timer.Start();
+            for (var k = 0; k < testArray.Length; k++)
+            {
+                int l = testArray[k];
+                results[k] = EvenBitwise(l);
+            }
+            timer.Stop();
+            Console.WriteLine("Bitwise took: " + timer.Elapsed);
+            timer.Reset();
+            resultTracker = results.ToList();
+        }
+    }
+
+    static int[] GenerateArray(int arrSize)
+    {
+        var retAr = new int[arrSize];
+        var seed = DateTime.Now.Ticks;
+        var random = new Random((int)(seed & 0x0000FFFF));
+        for (var i = 0; i < arrSize; i++)
+        {
+            var randomInt = random.Next();
+            if (numbers.Contains(randomInt))
+            {
+                i--;
+                continue;
+            }
+            numbers.Add(randomInt);
+            retAr[i] = random.Next();
+        }
+        return retAr;
+    }
+
+    static bool EvenBitwise(int testVal)
+    {
+        return (testVal & 1) == 0;
+    }
+
+    static bool EvenMod(int testVal)
+    {
+        return testVal % 2 == 0;
+    }
+}
---
+++ modvsbit.py
@@ -0,0 +1,31 @@
+# Mod vs Bitwise Python
+import time,random
+
+# Generate array
+def generate_array(arr_size):
+    return [random.randint(1,100000)] * 100000
+
+# Even bitwise
+def even_bitwise(num):
+    return (num & 1) == 0
+
+# Even modulus
+def even_modulus(num):
+    return (num % 2) == 0
+
+for i in range(1,10):
+    print("Generating Array")
+    testArray = generate_array(100000)
+
+    start_time_bit = time.time()
+    for i in testArray:
+        even_bitwise(i)
+    end_time_bit = time.time()
+
+    start_time_mod = time.time()
+    for i in testArray:
+        even_modulus(i)
+    end_time_mod = time.time()
+
+    print(f"Bitwise took:{(end_time_bit - start_time_bit):f}")
+    print(f"Modulus took:{(end_time_mod - start_time_mod):f}")
